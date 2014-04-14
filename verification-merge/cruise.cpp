c0fe2970df21c203ee3059cf468251c1faa31b80
shouponlin <shoupon.lin@gmail.com>
2014-04-14 17:28:00 -0400

    Component machine: Cruise(x)

--- verification-merge/cruise.cpp
+++ verification-merge/cruise.cpp
@@ -38,7 +38,7 @@ int Cruise::transit(MessageTuple *inMsg, vector<MessageTuple *> &outMsgs, bool &
                 return 3;
             }
             else
-                return 3;
+                return -1;
             break;
         case 1:
             if( msg == "RESET" ) {
@@ -46,9 +46,36 @@ int Cruise::transit(MessageTuple *inMsg, vector<MessageTuple *> &outMsgs, bool &
                 _state = 0 ;
                 return 3;
             }
-            else
+            else if( msg == "PILOT" ) {
+                assert(src == _name);
+                _state = 2;
                 return 3;
+            }
+            else
+                return -1;
             break;
+        default:
+            return -1;
+            break;
+    }
+}
+
+int Cruise::nullInputTrans(vector<MessageTuple *> &outMsgs, bool &high_prob, int startIdx)
+{
+    outMsgs.clear();
+    high_prob = true ;
+    if( startIdx != 0 )
+        return -1;
+    
+    switch (_state) {
+        case 2:
+            // models the driver deactivating collision avoidance mode
+            _state = 0;
+            outMsgs.push_back(new MessageTuple(0, machineToInt(_name),
+                                               0, messageToInt("DISENGAGE"), macId()));
+            return 3;
+            break;
+            
         default:
             return -1;
             break;
