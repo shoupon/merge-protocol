7b7cfd584598ed12b977cb4a35cae65e4b14962c
shouponlin <shoupon.lin@gmail.com>
2015-02-23 21:55:36 -0500

    remove old checker

--- mergechecker.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  mergechecker.cpp
-//  verification-merge
-//
-//  Created by Shou-pon Lin on 8/26/13.
-//  Copyright (c) 2013 Shou-pon Lin. All rights reserved.
-//
-
-#include "mergechecker.h"
